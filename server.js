bbf5db0f4ecf80b80fb10656f0164c05bcb0b315
Snypintyme <gavingytse@gmail.com>
2022-01-11 02:29:55 -0500

    fix broken images

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const DIST_DIR = path.join(__dirname, '/dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
 app.use(express.static(DIST_DIR));
+app.use(express.static("./src/assets/cards"));
 app.get('*', (req, res) => {
   res.sendFile(HTML_FILE);
 });
